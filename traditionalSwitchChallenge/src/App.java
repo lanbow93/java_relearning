4f3c3ad6c9a05849ea4bc103c5d3a74798a770ba
lanbow93 <the.doctor1501@gmail.com>
2024-08-28 10:19:38 -0500

    switch challenge completed

---
+++ traditionalSwitchChallenge/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    char letter = 'a';
+
+        switch(letter){
+            case 'a':
+                System.out.println("Able");
+                break;
+            case 'b':
+                System.out.println("Baker");
+                break;
+            case 'c':
+                System.out.println("Charlie");
+                break;
+            case 'd':
+                System.out.println("Dog");
+                break;
+            case 'e':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("Character Not Found");
+
+        }
+    }
+}
