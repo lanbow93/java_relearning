a23e430959f4970b559e8c117db134f3e6ca8408
lanbow93 <the.doctor1501@gmail.com>
2024-08-29 09:16:52 -0500

    fixed for loop to look at i and not number submitted

--- forStatementChallenge/src/App.java
+++ forStatementChallenge/src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + "a prime number.");
-        System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + "a prime number.");
-        System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + "a prime number.");
-        System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + "a prime number.");
-        System.out.println("15 is " + (isPrime(15) ? "" : "NOT ") + "a prime number.");
-        System.out.println("16 is " + (isPrime(16) ? "" : "NOT ") + "a prime number.");
+        // System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + "a prime number.");
+        // System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + "a prime number.");
+        // System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + "a prime number.");
+        // System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + "a prime number.");
+        // System.out.println("15 is " + (isPrime(15) ? "" : "NOT ") + "a prime number.");
+        // System.out.println("16 is " + (isPrime(16) ? "" : "NOT ") + "a prime number.");
+        countPrimes(50);
     }
     
     public static boolean isPrime(int number){
@@ -27,7 +28,8 @@ public class App {
         int primeCount = 0;
 
         for (int i = 2; i <= number; i++){
-            if(isPrime(number)){
+            if(isPrime(i)){
+                System.out.println(i + " is prime!");
                 primeCount++;
                 if(primeCount == 3){
                     break;
